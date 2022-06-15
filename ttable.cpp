2adb2b4bae16459724bd33b0528a42483ab6c552
Lovis Hagemyer <l_o_v_i_s@yahoo.de>
2022-06-15 16:10:40 +0200

    added insert and lookup functions

--- ttable.cpp
+++ ttable.cpp
@@ -2,8 +2,9 @@
 
 #include "ttable.h"
 
-TTable::Entry* TTable::table;
-int TTable::sizeInMiB;
+TTable::Entry* TTable::table = nullptr;
+int TTable::sizeInMiB = 0;
+
 
 void TTable::setSizeInMiB(int sizeInMiB) {
 
@@ -14,4 +15,27 @@ void TTable::setSizeInMiB(int sizeInMiB) {
         TTable::sizeInMiB = sizeInMiB;
         table = (Entry*) malloc(((uint64_t) 1048576) * ((uint64_t) sizeInMiB));
     }
+}
+
+TTable::Entry * TTable::lookup(uint64_t hash) {
+    uint64_t index = hash % ((1048576 / sizeof(Entry)) * sizeInMiB);
+
+    if(table[index].hash == hash) {
+        return &table[index];
+    } else {
+        return nullptr;
+    }
+}
+
+void TTable::insert(uint64_t hash, short eval, int nodeType, Game::Move move, int depth) {
+    uint64_t index = hash % ((1048576 / sizeof(Entry)) * sizeInMiB);
+
+
+    if(nodeType != 2)
+        table[index].move = move.compress();
+
+    table[index].hash = hash;    
+    table[index].eval = eval;
+    table[nodeType].entryType = nodeType;
+    table[index].depth = depth;
 }
--- ttable.h
+++ ttable.h
@@ -10,14 +10,23 @@ class TTable {
     public:
         struct Entry {
             uint64_t hash;
-            uint16_t flags;
-            uint16_t min;
-            uint16_t max;
+            uint16_t depth;
+            uint16_t entryType;
+            uint16_t eval;
             uint16_t move;
         };
 
         static Entry *lookup(uint64_t hash);
 
+        /**
+         * inserts an entry into the table, if the replacement scheme allows it.
+         * 
+         * @param hash the position hash
+         * @param eval the calculated evaluation
+         * @param nodeType 0: eval is a lower bound; 1: eval is an exact score; 2: eval is an upper bound
+         * @param move the best move. Only considered if nodeType is 0 or 1
+         * @param depth the search depth with which the position has been searched
+         */
         static void insert(uint64_t hash, short eval, int nodeType, Game::Move move, int depth);
 
         static void setSizeInMiB(int sizeInMiB);
